561c9218ceb7d07e3fdaef116149fefb989c1f53
rakeshpandey791 <rakesh.pandey@sjmsoftech.com>
2014-06-20 16:22:37 +0530

    test

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-	define('ENVIRONMENT', 'development');echo '12123';
+	define('ENVIRONMENT', 'development');echo 'test12123';
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
