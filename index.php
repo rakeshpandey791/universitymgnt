796f9728760cde38ddddad4b9ef84b838c516982
rakeshpandey791 <rakesh.pandey@sjmsoftech.com>
2014-06-20 16:15:51 +0530

    testing push

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-	define('ENVIRONMENT', 'development'); echo 'test';
+	define('ENVIRONMENT', 'development');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
